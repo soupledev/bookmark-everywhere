d7e5202f8240c7fbc1e8e659994b12d4d5689967
Theo Fontaine <github@souple.dev>
2026-05-10 23:29:49 +0200

    fix onboarding

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -41,8 +41,27 @@ export default defineBackground(() => {
       .then(() => true)
       .catch(() => false);
 
-    if (!didOpenDialog) return;
+    if (!didOpenDialog && !isWelcomePage(tab.url)) {
+      await browser.tabs.create({
+        url: browser.runtime.getURL("/newtab.html"),
+      });
+    }
 
     await markShortcutUsed();
   });
 });
+
+function isWelcomePage(tabUrl: string | undefined) {
+  if (!tabUrl) return false;
+
+  try {
+    const url = new URL(tabUrl);
+    const welcomeUrl = new URL(browser.runtime.getURL("/welcome.html"));
+
+    return (
+      url.origin === welcomeUrl.origin && url.pathname === welcomeUrl.pathname
+    );
+  } catch {
+    return false;
+  }
+}
